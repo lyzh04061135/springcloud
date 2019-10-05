28e4f1c6d5d0889ddeeb18041b5cf1241fd8e017
lyzh04061135 <zhucezhuanyonglyzh@163.com>
2019-10-05 16:49:04 +0800

    test idea

--- springclouddemo/src/main/java/com/jusfoun/springclouddemo/CloudDemo.java
+++ springclouddemo/src/main/java/com/jusfoun/springclouddemo/CloudDemo.java
@@ -1,7 +1,6 @@
 package com.jusfoun.springclouddemo;
 
-import org.springframework.boot.SpringApplication;  
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;  
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;  
 import org.springframework.web.bind.annotation.RequestMapping;  
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +13,15 @@ public class CloudDemo
     String home(){  
         return "Hello World!";  
           
-    }  
-	
-    public static void main(String[] args) throws Exception {  
-        SpringApplication.run(CloudDemo.class, args);  
+    }
+
+    @RequestMapping("/hello")
+    String hello(){
+        return "World!";
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDemo.class, args);
     }  
 }
